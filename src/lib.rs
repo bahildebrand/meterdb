e654f2300b3448131fb599f2ac9e81994490c76c
Blake Hildebrand <baugusthildebrand@gmail.com>
2025-08-26 17:06:23 -0400

    chore: RIP timmy

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,8 @@ mod db;
 #[cfg(any(feature = "std", test))]
 pub use block::StdTimestamp;
 pub use block::Timestamp;
-pub use db::Db as TimmyDb;
-pub use db::DbWriter as TimmyDbWriter;
+pub use db::Db as MeterDb;
+pub use db::DbWriter as MeterDbWriter;
 
 #[cfg(test)]
 extern crate std;
